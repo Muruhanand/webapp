21a50e3cd0f1bc41c6187a7a93a5db904379cdf3
Aizakku-ro <lolplays968@gmail.com>
2024-11-20 17:39:42 +0800

    tailwind config file

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./admin/**/*.{js,jsx,ts,tsx,html,php,vue,jsp}",],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
